1ac718f7355f69b1a3b6098d86fd2155544ee22c
AuTsing <763025696@qq.com>
2024-06-20 03:41:25 +0800

    0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "0.2.1"
+        versionName = "0.2.2"
         vectorDrawables {
             useSupportLibrary = true
         }
